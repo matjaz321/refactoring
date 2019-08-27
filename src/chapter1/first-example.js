2a0d6bcc7c18b51ca8222b4e53e4d47516f20186
matjaz321 <matjaz.zavski15@gmail.com>
2019-08-27 12:17:44 +0200

    First refactoring

--- src/chapter1/first-example.js
+++ src/chapter1/first-example.js
@@ -1,27 +1,27 @@
 'use strict';
 
 const amountFor = (performance, play) => {
-    let thisAmount = 0;
+    let result = 0;
 
     switch (play.type) {
         case 'tragedy':
-            thisAmount = 40000;
+            result = 40000;
             if (performance.audience > 30) {
-                thisAmount += 1000 * (performance.audience - 30);
+                result += 1000 * (performance.audience - 30);
             }
         break;
         case 'comedy':
-            thisAmount = 30000;
+            result = 30000;
             if (performance.audience > 20) {
-                thisAmount += 10000 + 500 * (performance.audience - 20);
+                result += 10000 + 500 * (performance.audience - 20);
             }
-            thisAmount += 300 * performance.audience;
+            result += 300 * performance.audience;
         break;
         default:
             throw new Error(`unkown type: ${play.type}`);
     }
 
-    return thisAmount;
+    return result;
 };
 
 export default function showResult(invoice, plays) {
@@ -38,14 +38,14 @@ export default function showResult(invoice, plays) {
     ).format;
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
+        let result = amountFor(perf, play);
 
         volumeCredits += Math.max(perf.audience - 30, 0);
         if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
-        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += `  ${play.name}: ${format(result/100)} (${perf.audience} seats)\n`;
     
-        totalAmount+= thisAmount;
+        totalAmount+= result;
     }
 
 
