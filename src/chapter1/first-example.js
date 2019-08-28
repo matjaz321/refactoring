e637cc499b4e485fddb437d67c94844d4ad94f7e
matjaz321 <matjaz.zavski15@gmail.com>
2019-08-28 12:38:23 +0200

    created new class private vraible _performances

--- src/chapter1/first-example.js
+++ src/chapter1/first-example.js
@@ -1,9 +1,19 @@
 'use strict';
 
 class PerformanceResult {
+
     constructor(plays, invoice) {
         this._plays = plays;
         this._invoice = invoice;
+        const self = this;
+        this._performances = invoice.performances.map(performance => this._enrichPerformance(performance, self));
+    }
+
+    _enrichPerformance(performance, self) {
+        const result = Object.assign({}, performance);
+        result.play = self._playFor(result);
+        result.amount = self._amountFor(result);
+        return result;
     }
 
     _playFor(performance) {
@@ -11,7 +21,7 @@ class PerformanceResult {
     }
 
     _amountFor(performance) {
-        const play = this._playFor(performance);
+        const play = performance.play;
         let result = 0;
     
         switch (play.type) {
@@ -38,7 +48,7 @@ class PerformanceResult {
     _volumeCreditsFor(performance) {
         let result = 0;
         result += Math.max(performance.audience - 30, 0);
-        if ('comedy' === this._playFor(performance).type) {
+        if ('comedy' === performance.play.type) {
             result += Math.floor(performance.audience / 5);
         }
         return result;
@@ -55,9 +65,9 @@ class PerformanceResult {
         ).format(number/100);
     }
 
-    _totalVolumeCredits(performances) {
+    _totalVolumeCredits() {
         let result = 0;
-        for (let perf of performances) {
+        for (let perf of this._performances) {
             result += this._volumeCreditsFor(perf);
         }
 
@@ -66,22 +76,26 @@ class PerformanceResult {
 
     _totalAmount() {
         let result = 0;
-        for (let perf of this._invoice.performances) {  
-            result += this._amountFor(perf);
+        for (let perf of this._performances) {  
+            result += perf.amount;
         }
 
         return result;
 
     }
 
-    showResult() {
+    _renderPlainText() {
         let result = `Statemnt for ${this._invoice.customer}\n`;
-        for (let perf of this._invoice.performances) {  
-            result += `  ${this._playFor(perf).name}: ${this._usd(this._amountFor(perf))} (${perf.audience} seats)\n`;
+        for (let perf of this._performances) {  
+            result += `  ${perf.play.name}: ${this._usd(perf.amount)} (${perf.audience} seats)\n`;
         }
         result += `Amount owed is ${this._usd(this._totalAmount())}\n`;
-        result += `You earned ${this._totalVolumeCredits(this._invoice.performances)} credits\n`;
+        result += `You earned ${this._totalVolumeCredits()} credits\n`;
         return result;
+    }
+
+    showResult() {
+        return this._renderPlainText();
     }
     
 }
